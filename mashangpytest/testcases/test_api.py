5e0c77566ba284486d127da35bff4ff941593edc
wangzixing <wangzixinghd@163.com>
2023-02-04 20:00:29 +0800

    .idea文件和测试test用例

--- mashangpytest/testcases/test_api.py
+++ mashangpytest/testcases/test_api.py
@@ -1,7 +1,18 @@
 import requests
 
-requests.get()
-requests.post()
-requests.put()
-requests.delete()
-requests.request()
+
+class TestApi:
+
+    def test_get_token(self):
+        url = "https://api.weixin.qq.com/cgi-bin/token"
+        data = {
+            "grant_type": "client_credential",
+            "appid": "wx8a9de038e93f77ab",
+            "secret": "8326fc915928dee3165720c910effb86"
+        }
+        res = requests.get(url, params=data)
+        print(res.json())
+
+
+if __name__ == '__main__':
+    TestApi.test_get_token()
