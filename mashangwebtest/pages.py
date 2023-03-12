37fbafaba43a0c3f5da92ed04cda9f645a5bac84
wangzixing <wangzixinghd@163.com>
2023-03-12 18:36:37 +0800

    PO模式实战

--- mashangwebtest/pages.py
+++ mashangwebtest/pages.py
@@ -5,10 +5,14 @@
 @Description:  定义PO
 """
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.expected_conditions import *
 
 
 class BasePage:
+
+    _loc_msg = '//p[@class="prompt-msg"]'
 
     def __init__(self, driver: webdriver.Chrome):
         self._driver = driver
@@ -26,5 +30,69 @@ class BasePage:
     def get_element(self, xpath):
         """元素定位，会自动进行等待"""
         el = self._wait.until(
-
+            visibility_of_element_located(  # 等待元素出现
+                (By.XPATH, xpath)
+            )
         )
+        return el
+
+    def alert_ok(self):
+        alert = self._wait.until(alert_is_present())    # 等待alert出现
+        alert.accept()
+
+
+class LoginPage(BasePage):
+    _loc_username = '//input[@placeholder="请输入用户名/手机/邮箱"]'
+    _loc_password = '//input[@placeholder="请输入登录密码"]'
+    _loc_btn = '//button[text()="登录"]'
+
+    def login(self, username, password):
+        self.username.send_keys(username)
+        self.password.send_keys(password)
+        self.btn.click()
+
+
+class AddressPage(BasePage):
+
+    _loc_new = '/html/body/div[4]/div[3]/div/div/button'
+    _loc_name = '/html/body/div[1]/form/div[2]/input'
+    _loc_tel = '/html/body/div[1]/form/div[3]/input'
+    _loc_province = '/html/body/div[1]/form/div[4]/div[1]'
+    _loc_city = '/html/body/div[1]/form/div[4]/div[2]'
+    _loc_district = '/html/body/div[1]/form/div[4]/div[3]'
+    _loc_address = '//*[@id="form-address"]'
+    _loc_btn = '/html/body/div[1]/form/div[7]/button'
+
+    def click_new(self):
+        self.new.click()
+        self.alert_ok()
+        iframe = self.get_element(  # 自动等待的定位
+            '//iframe[starts-with(@src, "http://101.34.221.219:8010")]')
+        self._driver.switch_to.frame(iframe)
+
+    @classmethod
+    def div_select(cls, element, value):
+        element.click()     # 显示下拉菜单
+        element.find_element(By.XPATH, './div/div/input').send_keys(value)
+        element.find_element(By.XPATH, './div/ul/li').click()
+
+    def input_info(self,
+                   name,
+                   tel,
+                   province,
+                   city,
+                   district,
+                   address):
+        self.name.send_keys(name)
+        self.tel.send_keys(tel)
+        # self.province.send_keys(province)
+        # self.city.send_keys(city)
+        # self.district.send_keys(district)
+        # 处理下拉选择框
+        self.div_select(self.province, province)
+        self.div_select(self.city, city)
+        self.div_select(self.district, district)
+        self.address.send_keys(address)
+
+    def click_save(self):
+        self.btn.click()
--- mashangwebtest/test_3.py
+++ mashangwebtest/test_3.py
@@ -10,6 +10,8 @@ import pytest
 from selenium.webdriver.common.by import By
 from webdriver_helper import get_webdriver
 
+from pages import LoginPage, AddressPage
+
 
 @pytest.fixture(scope='session')
 def driver():
@@ -79,23 +81,30 @@ def test_login_fail(driver):
     # 清除cookie，刷新页面
     driver.delete_all_cookies()
     driver.refresh()
+
+    # 1.打开页面，实例化Page
+    driver.get('http://101.34.221.219:8010/?s=user/logininfo.html')
+    page = LoginPage(driver)
+    # 2.调用page方法，完成交互
+    page.login("beifan", "123123")
+
     # 定位元素，模仿用户行为
-    driver.find_element(By.LINK_TEXT, "登录").click()
+    # driver.find_element(By.LINK_TEXT, "登录").click()
+    #
+    # driver.find_element(By.XPATH,
+    #                     '//input[@placeholder="请输入用户名/手机/邮箱"]'
+    #                     ).send_keys("beifan")
+    #
+    # driver.find_element(By.XPATH,
+    #                     '//input[@placeholder="请输入登录密码"]'
+    #                     ).send_keys("123123")
+    #
+    # driver.find_element(By.XPATH, '//button[text()="登录"]').click()
 
-    driver.find_element(By.XPATH,
-                        '//input[@placeholder="请输入用户名/手机/邮箱"]'
-                        ).send_keys("beifan")
-
-    driver.find_element(By.XPATH,
-                        '//input[@placeholder="请输入登录密码"]'
-                        ).send_keys("123123")
-
-    driver.find_element(By.XPATH, '//button[text()="登录"]').click()
-
-    time.sleep(1)  # 休眠1秒，等待系统加载
+    # time.sleep(1)  # 休眠1秒，等待系统加载
     # 获取系统提示
-    msg = driver.find_element(By.XPATH, '//p[@class="prompt-msg"]').text
-    assert msg == "密码错误"
+    # msg = driver.find_element(By.XPATH, '//p[@class="prompt-msg"]').text
+    assert page.msg.text == "密码错误"
 
 
 def test_order_success(user_driver):
@@ -120,3 +129,22 @@ def test_order_success(user_driver):
 
 def test_order_fail():
     pass
+
+
+def test_new_address(user_driver):
+    user_driver.get('http://101.34.221.219:8010/?s=useraddress/index.html')
+    page = AddressPage(user_driver)
+    page.click_new()    # 点击新增按钮
+    page.input_info(
+        "北凡",
+        "13812345678",
+        "湖南省",
+        "长沙市",
+        "岳麓区",
+        "开发南路183号"
+    )   # 输入内容
+    page.click_save()   # 点击保存
+
+    # user_driver.get_screenshot_as_file('a.png')
+
+    assert page.msg.text == '操作成功'
